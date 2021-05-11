a86730ec7cb2db27b8547be9cb4b81ad87b4fdf5
unknown <omarsefo7@gmail.com>
2021-05-11 17:09:59 +0300

    update

--- ber.js
+++ ber.js
@@ -4,7 +4,9 @@ var img = document.getElementById("star");
 
 btn1.addEventListener('click',()=>{
     img.src="imgb/img.jpg";
+    img.style.height="500px";
     setTimeout(()=>{
         img.src="imgb/VR.png";
+        img.style.height="634px";
         },5000)
 })
